4bc26dad958d2f22237714f26aa71ae10e4439bc
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-03-18 14:17:53 +0100

    text slider from some demo text implemented

--- slider.js
+++ slider.js
@@ -1,1 +1,19 @@
+// slider text
+const sliderText = ['Hello Slider', 'Hallo Mr Slider', 'Hi Mrs Slider'];
 
+//index of the array
+let index = 0;
+
+const sliderInner = document.getElementById('slider-inner');
+
+const sliderLoop = () => {
+  setInterval(() => {
+    if (index === sliderText.length) {
+      index = 0;
+    }
+    sliderInner.innerText = sliderText[index];
+    index++;
+  }, 2000);
+};
+
+sliderLoop();
